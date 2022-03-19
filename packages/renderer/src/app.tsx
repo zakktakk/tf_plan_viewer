21a3a6072eb2aae3221f5c08703b2f48bb820524
Takuro Yamazaki <takuro.yamazaki.w@gmail.com>
2022-03-19 14:08:33 +0900

    declare react module instead of Vue

---
+++ packages/renderer/src/app.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import './app.css'
+
+
+export const App: React.FC = () => {
+	const [count, setCount] = useState(0)
+
+	return (
+		<div className='app'>
+			<header className='app-header'>
+				<p>Vite + React</p>
+				<p>
+					<button onClick={() => setCount((count) => count + 1)}>
+						count is: {count}
+					</button>
+				</p>
+			</header>
+		</div>
+	)
+}
