53ad00a1c97f0b70721a90aeec144d574ea795e1
Takuro Yamazaki <takuro.yamazaki.w@gmail.com>
2022-03-19 17:11:59 +0900

    update: modify dev tools from vue to react

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -48,7 +48,7 @@ app.whenReady()
 if (import.meta.env.DEV) {
   app.whenReady()
     .then(() => import('electron-devtools-installer'))
-    .then(({default: installExtension, VUEJS3_DEVTOOLS}) => installExtension(VUEJS3_DEVTOOLS, {
+    .then(({default: installExtension, REACT_DEVELOPER_TOOLS}) => installExtension(REACT_DEVELOPER_TOOLS, {
       loadExtensionOptions: {
         allowFileAccess: true,
       },
